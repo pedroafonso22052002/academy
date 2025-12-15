0b9311ea3058bd41969bbebb52e789de3fdf955f
pedroafonso22052002 <pedroafonso.22052002@outlook.com>
2025-12-15 14:03:29 +0000

    removed duplicate base code folder

--- base_code/src/main/java/api/dto/request/CarRequest.java
+++
@@ -1,7 +0,0 @@
-package api.dto.request;
-
-import jakarta.annotation.Nullable;
-
-public record CarRequest(@Nullable Long id, @Nullable String brand, @Nullable String model, @Nullable Long price) {
-
-}
--- base_code/src/main/java/infrastructure/database/repository/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package infrastructure.database.repository;
-
-import infrastructure.database.entity.CarEntity;
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import jakarta.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class CarRepository implements PanacheRepository<CarEntity> {
-}
